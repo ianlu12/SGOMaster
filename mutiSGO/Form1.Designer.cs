87ba35f1f7abfc134b8f09e72305878ec853490a
Ian Lu <yangyi@pchome.tw>
2023-02-16 10:23:38 +0800

    加入專案檔案。

---
+++ mutiSGO/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace mutiSGO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.webView22 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.webView22)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(3, 32);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(368, 635);
+            this.webView21.TabIndex = 0;
+            this.webView21.ZoomFactor = 1D;
+            this.webView21.NavigationCompleted += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs>(this.webView21_NavigationCompleted);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "自動戰鬥";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(84, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "登出";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(377, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "自動戰鬥";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // webView22
+            // 
+            this.webView22.AllowExternalDrop = true;
+            this.webView22.CreationProperties = null;
+            this.webView22.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView22.Location = new System.Drawing.Point(377, 32);
+            this.webView22.Name = "webView22";
+            this.webView22.Size = new System.Drawing.Size(368, 635);
+            this.webView22.TabIndex = 5;
+            this.webView22.ZoomFactor = 1D;
+            this.webView22.NavigationCompleted += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs>(this.webView22_NavigationCompleted);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(458, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "登出";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 672);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.webView22);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webView21);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "mutiSGO";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.webView22)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView22;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ mutiSGO/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mutiSGO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+        }
+        async void init()
+        {
+            webView21.CreationProperties =
+            new Microsoft.Web.WebView2.WinForms.CoreWebView2CreationProperties();
+            webView21.CreationProperties.UserDataFolder = System.IO.Path.Combine(
+                Application.StartupPath, $"webView1");
+            await webView21.EnsureCoreWebView2Async();
+            webView21.CoreWebView2.Navigate("https://swordgale.online/");
+            webView22.CreationProperties =
+            new Microsoft.Web.WebView2.WinForms.CoreWebView2CreationProperties();
+            webView22.CreationProperties.UserDataFolder = System.IO.Path.Combine(
+                Application.StartupPath, $"webView2");
+            await webView22.EnsureCoreWebView2Async();
+            webView22.CoreWebView2.Navigate("https://swordgale.online/");
+
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            var viewHeight = this.Height - 75;
+            this.webView21.Height = viewHeight;
+            this.webView22.Height = viewHeight;
+            var viewWidth = (this.Width - 11 * 2 - 5 * (2 - 1)) / 2;
+            this.webView21.Width = viewWidth;
+            webView22.Left = this.webView21.Right + 5;
+            this.webView22.Width = viewWidth;
+            button3.Left = this.webView22.Left;
+            button4.Left = button3.Right + 5;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Enabled)
+            {
+                await webView21.CoreWebView2.ExecuteScriptAsync(@"javascript:setInterval(function(){document.getElementsByClassName('css-1myfyhp')[0].children[1].click();},11000);");
+
+            }
+            button1.Enabled = false;
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Enabled)
+            {
+                await webView22.CoreWebView2.ExecuteScriptAsync(@"javascript:setInterval(function(){document.getElementsByClassName('css-1myfyhp')[0].children[1].click();},11000);");
+            }
+            button3.Enabled = false;
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            await webView21.CoreWebView2.ExecuteScriptAsync(@"javascript:localStorage.clear()");
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            await webView22.CoreWebView2.ExecuteScriptAsync(@"javascript:localStorage.clear()");
+        }
+
+        private void webView21_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
+        {
+            if (!button1.Enabled)
+            {
+                button1.Enabled = true;
+            }
+
+        }
+
+        private void webView22_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
+        {
+            if (!button3.Enabled)
+            {
+                button3.Enabled = true;
+            }
+        }
+    }
+}
